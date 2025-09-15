75b33b26c5f782276e58caea00b510ec72d0938e
MrsCTR <camilatatiane.1994@gmail.com>
2025-09-15 17:58:10 -0300

    Questao02.py

---
+++ Revisão/Questao02.py
@@ -0,0 +1,4 @@
+'''
+Pode-se observar que o número, 125874, e seu duplo, 251748, contêm exatamente os mesmos dígitos, mas em uma ordem diferente.
+Encontre o menor inteiro positivo, x, de modo que 2x,3x,4x,5x, e 6x, contêm os mesmos dígitos.
+'''
