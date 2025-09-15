ba1384cf20b27956521b683d81f1715fb60acd53
MrsCTR <camilatatiane.1994@gmail.com>
2025-09-15 18:00:36 -0300

    Questao03.py

---
+++ Revisão/Questao03.py
@@ -0,0 +1,4 @@
+'''
+Encontre o total máximo de cima para baixo em triangle.txt (clique com o botão direito e 'Salvar link/destino como...'), 
+um arquivo de texto de 15K contendo um triângulo com cem linhas.
+'''
