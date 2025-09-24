acb28be7858e33790bb177aa9140d41856e092c8
MrsCTR <camilatatiane.1994@gmail.com>
2025-09-24 17:49:45 -0300

    exemplo02.py

--- Operações-Binárias/exemplo02.py
+++ Operações-Binárias/exemplo02.py
@@ -1,21 +1,29 @@
 import sys
 
 try:
-    strIP = int(input('Digite um endereço IP (Formato IPv4): '))
+   intValor = int(input('\nDigite um número inteiro: '))
 except ValueError:
-    sys.exit('Valor inválido. Por favor, informe um número inteiro.')
+   sys.exit('Valor inválido. Por favor, insira um número inteiro.')
 except KeyboardInterrupt:
-    sys.exit('\n Programa interrompido pelo usuário.')
+   sys.exit('\nPrograma interrompido pelo usuário.')
 except Exception as erro:
-    sys.exit(f'Erro inesperado: {erro}')
+   sys.exit(f'Erro inesperado: {erro}')
 else:
+   if intValor < 0:
+      sys.exit('Por favor, insira um número inteiro não negativo.')
 
-   lstOctetos = strIP.split('.')
+   # Calcular quantos bits são necessários para representar o número
+   intQtBits = intValor.bit_length()
 
-   if len(lstOctetos) != 4:
-       sys.exit('\n Erro: Formato IP inválido. Deve conter 4 octetos\n')
+   # Converter para binário e remover o prefixo '0b'
+   binValor = bin(intValor)[2:]  
 
+   if len(binValor) % 8 != 0:
+      # Calcular quantos zeros são necessários para completar o byte
+      intZeros = 8 - len(binValor) % 8
 
-   for octeto in lstOctetos:
-      if not octeto.isdigit() or not (0 <= int(octeto) <= 255):
-           
+      # Adicionar os zeros à esquerda
+      binValor = ('0' * intZeros) + binValor
+
+   # Exibir a saída
+   print(f'\n{intValor} em Binário....: 0b{binValor}\n')
