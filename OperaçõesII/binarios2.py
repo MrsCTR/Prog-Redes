febb0623a21844b31bd6baff47eee1ef3c44e4a8
MrsCTR <camilatatiane.1994@gmail.com>
2025-09-24 17:44:35 -0300

    binarios2.py

--- OperaçõesII/binarios2.py
+++ OperaçõesII/binarios2.py
@@ -1,3 +1,6 @@
-
+# Deslocamento binário
 intCIDR = 24 #255.255.255.0
-
+intMask = 0xFFFFFFFF
+intMask = intMask>>(32-intCIDR)
+intMask = intMask<<(32-intCIDR)
+print(intMask) # Imprime 4294967040 (4.294.967.040)
