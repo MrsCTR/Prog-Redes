6530670b1951986899af4d687a3a59a33ee87072
MrsCTR <camilatatiane.1994@gmail.com>
2025-09-24 17:48:07 -0300

    exemplo01.py

--- Operações-Binárias/exemplo01.py
+++ Operações-Binárias/exemplo01.py
@@ -1,24 +1,20 @@
 import sys
 
 try:
-    intvalor = int(input('Digite um número inteiro: '))
+   intValor = int(input('\nDigite um número inteiro: '))
 except ValueError:
-    sys.exit('Valor inválido. Por favor, informe um número inteiro.')
+   sys.exit('Valor inválido. Por favor, insira um número inteiro.')
 except KeyboardInterrupt:
-    sys.exit('\n Programa interrompido pelo usuário.')
+   sys.exit('\nPrograma interrompido pelo usuário.')
 except Exception as erro:
-    sys.exit(f'Erro inesperado: {erro}')
+   sys.exit(f'Erro inesperado: {erro}')
 else:
+   # Imprimir o valor binário do número inteiro
+   print(f'\n{intValor} em Binário....: {bin(intValor)}')
 
-    if intvalor < 0:
-        sys.exit('Informe um valor inteiro positivo:')
+   # Imprimir o valor hexadecimal do número inteiro
+   print(f'{intValor} em Hexadecimal: {hex(intValor)}')  
 
-    intQtbits = intvalor.bit_length()
-#converte para binário retirando '0b'
-    binvalor = bin(intvalor)[2:]
+   # Imprimir o valor octal do número inteiro
+   print(f'{intValor} em Octal......: {oct(intValor)}\n')
 
-    if len(binvalor) % 8 != 0:
-
-        intzeros = 8 - len(binvalor)
-#exibir a saída
-    print(f'\n {intvalor} em Binário.....: 0b{binvalor}\n')
