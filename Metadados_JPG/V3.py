5937429bf10fa3776e86df9eef6c4180fb5c469e
MrsCTR <camilatatiane.1994@gmail.com>
2025-10-08 18:03:15 -0300

    V3.py

---
+++ Metadados_JPG/V3.py
@@ -0,0 +1,34 @@
+#* VERSÃO2*
+#Metadados Lidos
+#-------------------------------------------------------------------------------
+{'TAGNumber': 256, 'DataFormat': 4, 'NumberComponents': 1, 'DataValue': 4080}
+{'TAGNumber': 257, 'DataFormat': 4, 'NumberComponents': 1, 'DataValue': 3072}
+{'TAGNumber': 271, 'DataFormat': 2, 'NumberComponents': 7, 'DataValue': 170}
+{'TAGNumber': 272, 'DataFormat': 2, 'NumberComponents': 9, 'DataValue': 177}
+{'TAGNumber': 274, 'DataFormat': 3, 'NumberComponents': 1, 'DataValue': 1}
+{'TAGNumber': 282, 'DataFormat': 5, 'NumberComponents': 1, 'DataValue': 186}
+{'TAGNumber': 283, 'DataFormat': 5, 'NumberComponents': 1, 'DataValue': 194}
+{'TAGNumber': 296, 'DataFormat': 3, 'NumberComponents': 1, 'DataValue': 2}
+{'TAGNumber': 305, 'DataFormat': 2, 'NumberComponents': 22, 'DataValue': 202}
+{'TAGNumber': 306, 'DataFormat': 2, 'NumberComponents': 20, 'DataValue': 224}
+{'TAGNumber': 531, 'DataFormat': 3, 'NumberComponents': 1, 'DataValue': 1}
+{'TAGNumber': 34665, 'DataFormat': 4, 'NumberComponents': 1, 'DataValue': 244}
+{'TAGNumber': 34853, 'DataFormat': 4, 'NumberComponents': 1, 'DataValue': 939}
+
+#* VERSÃO 3 *
+#Metadados Lidos
+#----------------------------------------------------------------------------------------------------
+{'TAGNumber': 'ImageWidth', 'DataFormat': 'Unsigned Long', 'NumberComponents': 1, 'DataValue': 4080}
+{'TAGNumber': 'ImageLength', 'DataFormat': 'Unsigned Long', 'NumberComponents': 1, 'DataValue': 3072}
+{'TAGNumber': 'Make', 'DataFormat': 'ASCII String', 'NumberComponents': 7, 'DataValue': 170}
+{'TAGNumber': 'YResolution', 'DataFormat': 'ASCII String', 'NumberComponents': 9, 'DataValue': 177}
+{'TAGNumber': 'Orientation', 'DataFormat': 'Unsigned Short', 'NumberComponents': 1, 'DataValue': 1}
+{'TAGNumber': 'XResolution', 'DataFormat': 'Unsigned Rational', 'NumberComponents': 1, 'DataValue': 186}
+{'TAGNumber': 'Unknown Tag', 'DataFormat': 'Unsigned Rational', 'NumberComponents': 1, 'DataValue': 194}
+{'TAGNumber': 'ResolutionUnit', 'DataFormat': 'Unsigned Short', 'NumberComponents': 1, 'DataValue': 2}
+{'TAGNumber': 'Software', 'DataFormat': 'ASCII String', 'NumberComponents': 22, 'DataValue': 202}
+{'TAGNumber': 'DateTime', 'DataFormat': 'ASCII String', 'NumberComponents': 20, 'DataValue': 224}
+{'TAGNumber': 'YCbCrPositioning', 'DataFormat': 'Unsigned Short', 'NumberComponents': 1, 'DataValue': 1}
+{'TAGNumber': 'ExifOffset', 'DataFormat': 'Unsigned Long', 'NumberComponents': 1, 'DataValue': 244}
+{'TAGNumber': 'GPSInfo', 'DataFormat': 'Unsigned Long', 'NumberComponents': 1, 'DataValue': 939}
+
