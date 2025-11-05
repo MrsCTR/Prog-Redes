bde764b4079954f44ec3bc1e79aab863b90c7df3
MrsCTR <camilatatiane.1994@gmail.com>
2025-11-05 17:11:37 -0300

    Questao04.py

---
+++ Aula03-11/HTTP.py
@@ -0,0 +1,20 @@
+import requests
+
+# Definindo a URL para requisição
+strURL = 'http://www.globo.com'
+
+#Definição de cabeçalhos HTTP
+Headers = { 'User-Agent': 'MeuAppPython/1.0' }
+
+ # Fazendo a requisição GET
+try:
+   response = requests.get(strURL, headers=Headers)
+except requests.exceptions.ConnectTimeout:
+   print(f'\nERRO: A conexão demorou demais (Timeout). O servidor pode estar offline ou lento...\n')
+except requests.exceptions.RequestException as e:
+   print(f'\nERRO: Ocorreu um erro ao fazer a requisição: {e}\n')
+except Exception as e:
+   print(f'\nERRO: {e}\n')
+
+else:
+   
---
+++ Revisão/Questao04.py
@@ -0,0 +1,9 @@
+'''
+Baixe o arquivo e seu hash associado.Confira se o hash do arquivo confere.
+Qual o candidato a Presidente com mais votos?
+Qual o candidato a Presidente com mais votos em cada município?
+Qual o candidato a Governador mais votado?
+Quais os deputados a deputado federal tiveram mais que 6% dos votos?
+Quais os dez candidatos a Deputado Federal mais votados?
+Quantos votos a deputado federal teve cada partido?
+'''
