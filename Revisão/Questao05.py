ae49ea7b00b8577d0fef9f6c9ed3622578edaa1b
MrsCTR <camilatatiane.1994@gmail.com>
2025-11-18 14:23:13 -0300

    Questao05.py

---
+++ Revisão/Questao05.py
@@ -0,0 +1,5 @@
+'''
+Faça um programa que informa o número mínimo de cédulas de cada valor de real que serão devolvidas a um cliente de um restaurante que pagou uma conta. 
+O programa recebe o valor da conta e o valor que o cliente pagou; 
+exibe o número de cédulas de cada valor que deve ser dado ao cliente.
+'''
