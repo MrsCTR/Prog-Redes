863e08cf59259ac4cc61997ae18d39f563760dac
Mrs.CTR <camilatatiane.1994@gmail.com>
2025-11-24 17:57:40 -0300

    servidor.py

---
+++ exercicio24-11/servidor.py
@@ -0,0 +1,43 @@
+import socket
+
+# ----------------------------------------------------------------------
+HOST_IP_SERVER  = ''  # Definindo o IP do servidor
+HOST_PORT       = 50000           # Definindo a porta
+
+BUFFER_SIZE     = 512             # Tamanho do buffer
+CODE_PAGE       = 'utf-8'         # Definindo a página de 
+                                  # codificação de caracteres
+# ----------------------------------------------------------------------
+
+
+# Criando o socket (socket.AF_INET -> IPV4 / socket.SOCK_DGRAM -> UDP)
+sockServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Ligando o socket à porta
+sockServer.bind( (HOST_IP_SERVER, HOST_PORT) ) 
+
+# Definindo Timeout para o socket
+sockServer.settimeout(0.5)
+
+print('\nRecebendo Mensagens...\n\n')
+print('Pressione CTRL+C para sair do servidor...\n\n')
+print('.'*100 + '\n')
+
+try:
+    while True:
+       try:
+       # Recebendo os dados do cliente
+          byteMensagem, tuplaCliente = sockServer.recvfrom(BUFFER_SIZE)
+       except socket.timeout:
+           continue
+       else: # Obtendo nome do Host
+          strHostName = socket.gethostbyaddr(tuplaCliente[0])[0]
+          strHostName = strHostName.split('.')[0].upper()
+        # Imprimindo a mensagem recebida convertendo de bytes para string
+          print(f'{tuplaCliente} -> {strHostName}: {byteMensagem.decode(CODE_PAGE)}')
+except KeyboardInterrupt:
+    print('\nAVISO: Foi pressionado CTRL+C...\nSaindo do servidor')
+finally:
+    # Fechando o socket
+    sockServer.close()
+    print('Servidor finalizado com sucesso...\n\n')
